b2ac79185ded9463acf8f2a8a8e931daa5ee0b05
La Ode Muhammad Rahmatullah Baido <101871429+Lucerrr@users.noreply.github.com>
2022-06-09 18:42:38 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -50,4 +50,54 @@ void array::bubblesort(){
   for(int i=0; i<jumlah_data; i++){
     for(int j=0; j<jumlah_data-i-1; j++){
       if(kode[j]>kode[j+1]){
+        temp = kode[j];
+        kode[j]=kode[j+1];
+        kode[j+1]=temp;
 
+        temp1=lulus[j][0];
+        lulus[j][0]=lulus[j+1][0];
+        lulus[j+1][0]=temp1;
+
+        temp2=lulus[j][0];
+        lulus[j][0]=lulus[j+1][0];
+        lulus[j+1][0]=temp2;
+
+        temp3=lulus[j][0];
+        lulus[j][0]=lulus[j+1][0];
+        lulus[j+1][0]=temp3;
+        
+        temp4=lulus[j][0];
+        lulus[j][0]=lulus[j+1][0];
+        lulus[j+1][0]=temp4;
+
+        temp5=lulus[j][0];
+        lulus[j][0]=lulus[j+1][0];
+        lulus[j+1][0]=temp5;
+      }
+    }
+  } 
+}
+
+void array::proses(){
+  bubblesort();
+}
+
+void array::output(){
+  cout<<"------------------------------------------------------------------------------------------"<<endl;
+  cout<<"Tahun Prodi  |"<<"Komputer   |"<<"Elektro    |"<<"Pemesinan  |"<<"Rata-rata kelulusan pertahun semua prodi"<<endl;
+  cout<<"------------------------------------------------------------------------------------------"<<endl;
+  for(int i=0;i<jumlah_data;i++){
+    for(int j=0;j<5;j++){
+      cout<<lulus[i][j]<<"\t\t |";
+      }
+    cout<<endl;
+    }
+}
+
+
+int main() {
+  array a;
+  a.input();
+  a.proses();
+  a.output();
+}
